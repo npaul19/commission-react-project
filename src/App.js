6fecc96da3d1e107204761e6a08f7d9656380538
harishpaul1811 <92162589+harishpaul1811@users.noreply.github.com>
2021-10-08 18:10:59 +0530

    Add basic card in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from "./components/UI/Card/Card";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Card>This is a basic card component.</Card>
         <a
           className="App-link"
           href="https://reactjs.org"
