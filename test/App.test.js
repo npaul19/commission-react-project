184755603a180e0c94548465d68ec2279f294390
Nishtha Paul <76488641+npaul19@users.noreply.github.com>
2022-10-01 00:48:40 +0530

    Add test to render welcome text

--- test/App.test.js
+++ test/App.test.js
@@ -1,3 +1,8 @@
 it("renders without crashing", () => {
   shallow(<App />);
 });
+it("renders Welcome text", () => {
+  const wrapper = shallow(<App />);
+  const welcome = "Learn React From Google.com";
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
